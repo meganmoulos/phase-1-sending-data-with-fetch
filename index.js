8287c2258cec8cd2650a1b02bb23b15e6ea32bff
Megan <meganmoulos@gmail.com>
2022-08-14 16:13:09 -0700

    commit tree 30daf2051f065279532e52fc4ecb160c68bb9120

--- index.js
+++ index.js
@@ -1,1 +1,33 @@
 // Add your code here
+
+function submitData(userName, userEmail){
+    let options = {
+        method: 'POST',
+        headers: {
+            'Content-Type': 'application/json',
+            'Accept': 'application/json'
+        },
+        body: JSON.stringify(
+            {
+                name : userName, 
+                email: userEmail
+            }
+        )
+    };
+    
+    return fetch('http://localhost:3000/users', options)
+        .then(res => res.json())
+        .then(obj => {
+           let idNum = obj.id
+           let elem = document.createElement('p')
+           elem.textContent = idNum
+           document.body.appendChild(elem)
+        })
+        .catch(error => {
+            let elem = document.createElement('p')
+            elem.textContent = error.message
+            document.body.appendChild(elem)
+        })
+}
+
+submitData('Giuliano', 'Gmail')
